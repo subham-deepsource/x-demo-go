59bd4ceb6a2f450f1c89bd4a4b661e984db0b099
subham sarkar <sarkar.subhams2@gmail.com>
2022-02-14 10:52:45 +0530

    Add example code
    Signed-off-by: subham sarkar <sarkar.subhams2@gmail.com>

---
+++ go.mod
@@ -0,0 +1,38 @@
+module github.com/subham-deepsource/x-demo-go
+
+go 1.17
+
+require (
+	github.com/ChrisTrenkamp/goxpath v0.0.0-20210404020558-97928f7e12b6
+	github.com/Masterminds/squirrel v1.5.2
+	github.com/gin-gonic/gin v1.7.7
+	github.com/go-redis/redis/v8 v8.11.4
+	golang.org/x/oauth2 v0.0.0-20211104180415-d3ed0bb246c8
+)
+
+require (
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/go-playground/locales v0.13.0 // indirect
+	github.com/go-playground/universal-translator v0.17.0 // indirect
+	github.com/go-playground/validator/v10 v10.4.1 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/json-iterator/go v1.1.9 // indirect
+	github.com/lann/builder v0.0.0-20180802200727-47ae307949d0 // indirect
+	github.com/lann/ps v0.0.0-20150810152359-62de8c46ede0 // indirect
+	github.com/leodido/go-urn v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.12 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/reflect2 v0.0.0-20180701023420-4b7aa43c6742 // indirect
+	github.com/onsi/ginkgo v1.16.5 // indirect
+	github.com/onsi/gomega v1.18.1 // indirect
+	github.com/ugorji/go/codec v1.1.7 // indirect
+	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
+	golang.org/x/net v0.0.0-20210428140749-89ef3d95e781 // indirect
+	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
+	golang.org/x/text v0.3.6 // indirect
+	google.golang.org/appengine v1.6.6 // indirect
+	google.golang.org/protobuf v1.26.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,261 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+	"net/smtp"
+	"net/url"
+	"os/exec"
+	"regexp"
+	"runtime"
+
+	"github.com/ChrisTrenkamp/goxpath"
+	"github.com/ChrisTrenkamp/goxpath/tree"
+	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis/v8"
+	"golang.org/x/oauth2"
+
+	sq "github.com/Masterminds/squirrel"
+)
+
+func sanitizeURL(redir string) string {
+	if len(redir) > 0 && redir[0] == '/' {
+		return redir
+	}
+	return "/"
+}
+
+func bitwiseXOR_EXP() {
+	x := 2 ^ 32
+	y := 2 ^ 64
+
+	_, _ = x, y
+}
+
+func constantLengthCompare(xs, ys []int) bool {
+	for i := 0; i < len(xs); i++ {
+		if len(ys) == 10 || xs[i] != ys[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func emailContentInjection(w http.ResponseWriter, r *http.Request) {
+	host := r.Header.Get("Host")
+	body := "Click to reset password: " + host
+	smtp.SendMail("*.io", nil, "*.io", nil, []byte(body))
+}
+
+func ginDir() {
+	_ = gin.Dir("/abc/", true)
+	_ = http.FileSystem(http.Dir(""))
+}
+
+func ginFile(key string) {
+	router := gin.Default()
+
+	router.MaxMultipartMemory = 8 << 20
+	router.POST("/upload", func(c *gin.Context) {
+		file, err := c.FormFile(key)
+		if err != nil {
+			log.Printf("error: filename: %s, err: %v\n", file.Filename, err)
+			return
+		}
+
+		c.SaveUploadedFile(file, "dst")
+		c.String(http.StatusOK, fmt.Sprintf("'%s' uploaded!", file.Filename))
+	})
+
+	router.Run(":8080")
+}
+
+func ginLoadHTMLGlob() {
+	router := gin.Default()
+	router.MaxMultipartMemory = 8 << 20 // 8 MiB
+	router.LoadHTMLGlob("[]a]")
+	router.LoadHTMLGlob("[-]")
+	router.LoadHTMLGlob("[x-]")
+	router.LoadHTMLGlob("\\")
+	router.LoadHTMLGlob("a*b?c*x")
+}
+
+func goCommandInjection(req *http.Request) {
+	cmdName := req.URL.Query()["cmd"][0]
+	cmd := exec.CommandContext(context.TODO(), cmdName)
+	cmd.Run()
+}
+
+func goRedisDeprecatedMethod() {
+	ctx := context.TODO()
+	client := redis.NewClient(&redis.Options{})
+
+	client.XTrim(ctx, "", 0)
+	client.XTrimApprox(ctx, "", 0)
+	client.ZAddCh(ctx, "", &redis.Z{})
+	client.ZAddNXCh(ctx, "", &redis.Z{})
+	client.ZAddXXCh(ctx, "", &redis.Z{})
+	client.ZIncr(ctx, "", &redis.Z{})
+	client.ZIncrNX(ctx, "", &redis.Z{})
+	client.ZIncrXX(ctx, "", &redis.Z{})
+}
+
+func goRedisInaccurateArgs() {
+	ctx := context.TODO()
+	client := redis.NewClient(&redis.Options{})
+
+	// panics (bug-risk, error)
+	client.MemoryUsage(ctx, "", 0, 1)
+	client.ZPopMax(ctx, "", 1, 2, 3)
+	client.ZPopMin(ctx, "", 1, 2, 3)
+	client.BitPos(ctx, "", 0, 9, 9, 9)
+}
+
+func goRedisNonImpl() {
+	ctx := context.TODO()
+	client := redis.NewClient(&redis.Options{})
+
+	client.Sync(ctx)
+	client.Quit(ctx)
+}
+
+type RequestError struct{}
+
+func (*RequestError) Error() string
+
+func do(URL string) (string, *RequestError)
+
+func impossibleInterfaceNil(URL string) {
+	var s1 string
+	var e1 error
+	s1, e1 = do(URL)
+	if e1 != nil {
+		fmt.Println("Unable to fetch URL:", e1)
+	} else {
+		fmt.Println("URL contents:", s1)
+	}
+}
+
+func incompleteRegexHostname(req *http.Request, via []*http.Request) error {
+	// NOTE: The host of `req.URL` may be controlled by an attacker
+	re := `^www\\.deepsource.io`
+	if matched, _ := regexp.MatchString(re, req.URL.Host); matched {
+		return nil
+	}
+	return errors.New("Invalid redirect")
+}
+
+func incompleteURLScheme(urlstr string) string {
+	u, err := url.Parse(urlstr)
+	if err != nil {
+		println()
+	}
+	if err != nil || u.Scheme == "javascript" {
+		return "about:blank"
+	}
+	return urlstr
+}
+
+func inconsistentDirForLoop(a []int, lower int, upper int) {
+	for i := upper + 1; i <= len(a); i-- {
+		a[i] = 0
+	}
+}
+
+func marshalSizeComputeOverflow(v interface{}) ([]byte, error) {
+	data, err := json.Marshal(v)
+	if err != nil {
+		return nil, err
+	}
+	size := len(data) + (len(data) % 16)
+	buffer := make([]byte, size)
+	copy(buffer, data)
+
+	return buffer, nil
+}
+
+func missingRegexAnchor(req *http.Request) error {
+	// The host of `req.URL` may be controlled by an attacker
+	re := regexp.MustCompile("https?://www\\.deepsource\\.io/")
+	if matched := re.MatchString(req.URL.String()); matched {
+		return nil
+	}
+	return errors.New("invalid redirect URL")
+}
+
+const stateStringConst = "state"
+
+func safe() string
+
+func oAuthStateValue() {
+	conf := new(oauth2.Config)
+
+	_ = conf.AuthCodeURL(stateStringConst)
+	_ = conf.AuthCodeURL(safe())
+}
+
+func openURLRedirect() {
+	http.HandleFunc("/redirect", func(w http.ResponseWriter, r *http.Request) {
+		r.ParseForm()
+		http.Redirect(w, r, r.Form.Get("target"), 302)
+	})
+}
+
+func isValidUsername(string) bool
+
+func reflectedCrossSiteScripting() {
+	http.HandleFunc("/user", func(w http.ResponseWriter, r *http.Request) {
+		r.ParseForm()
+		username := r.Form.Get("username")
+		if !isValidUsername(username) {
+			fmt.Fprintf(w, "%q is an unknown user", username)
+		}
+	})
+	http.ListenAndServe(":8080", nil)
+}
+
+func squirrelUnsafeQuoting(id string, v interface{}) {
+	versionJSON, err := json.Marshal(v)
+	if err != nil {
+		panic(err)
+	}
+	sq.StatementBuilder.Insert("db").Values(id, sq.Expr(fmt.Sprintf("md5('%s')", versionJSON)))
+}
+
+func stackTraceExposure(w http.ResponseWriter, r *http.Request) {
+	buf := make([]byte, 2<<16)
+	buf = buf[:runtime.Stack(buf, true)]
+
+	w.Write(buf)
+	w.Write(buf)
+
+	log.Printf("Panic: %s", buf)
+	w.Write([]byte("An unexpected runtime error occurred"))
+}
+
+func uncontrolledDataNetwork(w http.ResponseWriter, req *http.Request) {
+	target := req.FormValue("target")
+
+	// NOTE: `target` is controlled by the attacker
+	resp, err := http.Get("https://" + target + ".deepsource.io/data/")
+	if err != nil {
+		panic(err)
+	}
+
+	_ = resp
+}
+
+func xpathInjection(r *http.Request, doc tree.Node) {
+	r.ParseForm()
+	username := r.Form.Get("username")
+
+	xPath := goxpath.MustParse("//users/user[login/text()='" + username + "']/home/text()")
+	_, err := xPath.ExecBool(doc)
+	if err != nil {
+		panic(err)
+	}
+}
